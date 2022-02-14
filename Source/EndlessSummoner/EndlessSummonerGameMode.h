6a6697afc17c0c97b5b6b013de5aec5bdd3108ab
Killian ML <killian.motel@gmail.com>
2022-02-14 13:25:52 +0100

    first datas

---
+++ Source/EndlessSummoner/EndlessSummonerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EndlessSummonerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEndlessSummonerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEndlessSummonerGameMode();
+};
+
+
+
